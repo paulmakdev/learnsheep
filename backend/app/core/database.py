3221f20b2dc10fa7e2fad5ba0ea67967a2a7a1ee
paulmakdev <pdmakarev@gmail.com>
2026-05-06 20:05:21 -0400

    ADR-00009 | Added connection recycling after certain amount of time.

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.database_url)
+engine = create_engine(settings.database_url, pool_pre_ping=True, pool_recycle=1800)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
